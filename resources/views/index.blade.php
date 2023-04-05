9ef98f7ce1661e377aeb3e8b2c6e1b6abe13ac9d
tlaxcalteco <pedro_1320114068@uptecamac.edu.mx>
2023-04-05 01:44:27 -0600

    View

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NailsDreamSG</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+   <header id="header">
+        <img src="/assets/img/icon/logo.jpg" alt="logo" class='container-logo'>
+        <div class='container-menu'>
+            <h1>NailsDreamSG</h1>
+            <nav class='menus'>
+                <ul>
+                    <li><a href="#">Uñas</a></li>
+                    <li><a href="#">Maquillaje</a></li>
+                    <li><a href="#">pedicure</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ViewController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// ----------------------View index-----------------------------
+Route::resource('inicio', ViewController::class)->names('index');
